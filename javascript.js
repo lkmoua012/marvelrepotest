3252f4038ce1f50accd32db83227f23bf2710402
larry.moua <lkmoua@gmail.com>
2018-07-21 10:55:51 -0700

    changed html

--- javascript.js
+++ javascript.js
@@ -1,14 +1,18 @@
 $(function() {
     console.log( 'ready!' );
 
+    var charName = "";
+
     var queryURL = "https://gateway.marvel.com:443/v1/public/characters?name=daredevil&apikey=3729b58ef60ba8affe97db66144af1c2";
 
     $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
+        var thumbnail = (response.data[4].thumbnail[0] + response.data[4].thumbnail[1]);
         console.log(response);
         $(".charName").append(response.data[4].name);
+        $(".charName").append(thumbnail);
     
       });
       //^ End of AJAX
